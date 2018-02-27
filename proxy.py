35b63a3bd09041d3eb2929ef10d4806e67461bda
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-02-27 13:19:30 +0300

    finalized proxy

--- proxy.py
+++ proxy.py
@@ -1,6 +1,7 @@
 #!.env/bin/python
 
 import logging
+import json
 from websocket_server import WebsocketServer
 from threading import Lock
 
@@ -12,6 +13,7 @@ def new_client(client, server):
 	GLOBAL_LOCK.acquire()
 	try:
 		ALL_CLIENTS[client["id"]] = client
+		print ("PROXY: Client connected: {} {}".format(client["id"], client["address"]))
 	except Exception as e:
 		print (e)
 	finally:
@@ -22,6 +24,7 @@ def client_left(client, server):
 	GLOBAL_LOCK.acquire()
 	try:
 		del ALL_CLIENTS[client["id"]]
+		print ("PROXY: Client disconnected: {}".format(client["id"]))
 	except Exception as e:
 		print (e)
 	finally:
@@ -31,11 +34,17 @@ def on_message(client, server, message):
 	global ALL_CLIENTS, GLOBAL_LOCK
 	GLOBAL_LOCK.acquire()
 	try:
+		if '127.0.0.1' not in client["address"][0] and 'localhost' not in client["address"][0] and '0.0.0.0' not in client["address"][0] and '192.168.10.102' not in client["address"][0]:
+			message = json.loads(message)
+			message["__room_id"] = "1"
+			message = json.dumps(message)
+		#print ("PROXY: [{}]: {}".format(client["id"], message))
 		for clid in list(ALL_CLIENTS.keys()):
 			if clid != client["id"]:
 				server.send_message(ALL_CLIENTS[clid], message)
+				print ("PROXY: [{} => {}]".format(client["id"], clid))
 	except Exception as e:
-		print (e)
+		print ("ERROR IN FORWARDING ", e)
 	finally:
 		GLOBAL_LOCK.release()
 
